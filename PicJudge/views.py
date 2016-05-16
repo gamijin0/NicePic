82c20b419bb3c16386e6d6f15625a936f4d66e49
ChaosKin <497386856@qq.com>
2016-05-16 13:51:17 +0800

    index_view

--- PicJudge/views.py
+++ PicJudge/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import os
 
 # Create your views here.
+
+def home(request):
+    IMG_list = os.listdir("/home/ftp/MyPic")
+    return render(request,'index.html',{'IMG_list':IMG_list})
